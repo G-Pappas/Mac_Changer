bd4020a1d16a4d2b91b6313b5eaa3a2ee16ae5d0
G-Pappas <xgpappas@outlook.com>
2020-09-29 07:00:39 -0400

    Added basic manu, reset to default mac function, object-oriented convertion

--- main.py
+++ main.py
@@ -1,20 +1,52 @@
 #!/usr/bin/env Python
-
 import subprocess
 
-# Ask user which interface he want to interact with.
-int_name = input("Enter interface name to interact:\n ")
-# Ask user for mac address.
-new_mac = input("Enter new mac address:\n ")
+
+def ask_for_input():
+    # Ask user which interface he want to interact with.
+    ask_for_input.inter_name = str(input("Enter interface name to interact:\n "))
+    # Ask user for mac address.
+    ask_for_input.new_mac = str(input("Enter new mac address:\n "))
 
 
+# Function to change the mac address.
 def change_mac():
     # Turn the interface I want to interact with down.
-    subprocess.call(f'ifconfig {int_name} down', shell = True)
+    subprocess.call(["ifconfig", ask_for_input.inter_name, "down"])
     # Change mac.
-    subprocess.call(f'ifconfig {int_name} hw ether {new_mac}', shell = True)
+    subprocess.call(["ifconfig", ask_for_input.inter_name, "hw", "ether", ask_for_input.new_mac])
     # After making the changes I want turn the interface up again.
-    subprocess.call(f'ifconfig {int_name} up', shell = True)
+    subprocess.call(["ifconfig", ask_for_input.inter_name, "up"])
 
 
-change_mac()
+# Function to reset mac value to default state.
+def reset_mac():
+    subprocess.call("ifconfig eth0 down", shell = True)
+    subprocess.call("ifconfig eth0 up", shell = True)
+    subprocess.call("sudo ifconfig eth0 hw ether $(ethtool -P eth0 | awk '{print $3}')", shell = True)
+    subprocess.call("ifconfig eth0 down", shell = True)
+    subprocess.call("ifconfig eth0 up", shell = True)
+
+
+# Function to browse in the program.
+def user_greet():
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("To change mac address press 1")
+    print("To reset default mac value press 2")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    user_choice = input()
+    if user_choice == "1":
+        ask_for_input()
+        change_mac()
+    elif user_choice == "2":
+        reset_mac()
+    else:
+        print("\n")
+        print("\n")
+        print("************************************")
+        print("*    Please chose option 1 or 2    *")
+        print("************************************")
+        user_greet()
+
+
+user_greet()
